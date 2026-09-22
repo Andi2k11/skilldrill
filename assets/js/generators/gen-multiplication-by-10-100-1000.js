a4b051116321ed756070b0855ded1ab21bc10fbc
Andreas Eriksson <andreas@erikssonnet.se>
2026-09-22 18:09:49 +0200

    Rename generators to gen- prefix and add alias files

---
+++ assets/js/generators/gen-multiplication-by-10-100-1000.js
@@ -0,0 +1,8 @@
+// gen-multiplication-by-10-100-1000.js (alias -> gen-multiplication-generator)
+window.multiplicationGenerators = window.multiplicationGenerators || {};
+if(!window.multiplicationGenerators['multiplication-by-10-100-1000']){
+  var s = document.createElement('script');
+  s.src = '/assets/js/generators/gen-multiplication-generator.js';
+  s.async = false;
+  document.head.appendChild(s);
+}
