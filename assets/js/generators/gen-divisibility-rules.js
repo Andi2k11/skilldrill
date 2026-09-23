3b4be27a01853a9612e15c2f3f82ad0cf25844d2
Andreas Eriksson <andreas@erikssonnet.se>
2026-09-23 09:20:48 +0200

    feat: add shuffleAnswers setting to exercise JSON and generator

--- assets/js/generators/gen-divisibility-rules.js
+++ assets/js/generators/gen-divisibility-rules.js
@@ -32,6 +32,8 @@ window.divisibilityGenerators['divisibility-rules'] = (function () {
         ? params.divisors
         : [2, 3, 5, 10];
 
+      var shuffleAnswers = (typeof params.shuffleAnswers === 'boolean') ? params.shuffleAnswers : true;
+
       while (questions.length < count) {
 
         var number = randInt(min, max);
@@ -45,72 +47,14 @@ window.divisibilityGenerators['divisibility-rules'] = (function () {
         if (correctAnswers.length === 0) {
           continue;
         }
+
+        var opts = divisors.map(String);
+        if (shuffleAnswers) opts = shuffle(opts);
 
         questions.push({
           number: number,
           text: number,
-          options: shuffle(divisors.map(String)),
-          answer: correctAnswers
-        });
-      }
-
-      return questions;
-    }
-  };
-
-})();
-window.divisibilityGenerators = window.divisibilityGenerators || {};
-
-window.divisibilityGenerators['divisibility-rules'] = (function () {
-
-  function randInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function shuffle(array) {
-    var a = array.slice();
-
-    for (var i = a.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var tmp = a[i];
-      a[i] = a[j];
-      a[j] = tmp;
-    }
-
-    return a;
-  }
-
-  return {
-    generate: function (params) {
-
-      var questions = [];
-      var count = params.questionCount || 15;
-
-      var min = typeof params.min === 'number' ? params.min : 1;
-      var max = typeof params.max === 'number' ? params.max : 399;
-
-      var divisors = params.divisors && params.divisors.length
-        ? params.divisors
-        : [2, 3, 5, 10];
-
-      while (questions.length < count) {
-
-        var number = randInt(min, max);
-
-        var correctAnswers = divisors
-          .filter(function (d) {
-            return number % d === 0;
-          })
-          .map(String);
-
-        if (correctAnswers.length === 0) {
-          continue;
-        }
-
-        questions.push({
-          number: number,
-          text: 'Vilka delbarhetsregler fungerar på talet ' + number + '?',
-          options: shuffle(divisors.map(String)),
+          options: opts,
           answer: correctAnswers
         });
       }
